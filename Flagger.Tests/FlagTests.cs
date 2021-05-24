6d1095b3398bcaf77e807926fcf1feb8aa099113
yellowsink <yellowsink@protonmail.com>
2021-05-24 22:14:42 +0100

    Start point, must add more tests and a readme.

---
+++ Flagger.Tests/FlagTests.cs
@@ -0,0 +1,100 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Flagger.Tests
+{
+	public class FlagTests
+	{
+		[SetUp]
+		public void Setup() { }
+
+		[Test]
+		public void ArrayCreateTest()
+		{
+			var flagCollection = new FlagCollection
+				(new []
+				{
+					('f', false),
+					('h', true),
+					('g', true),
+					('i', false)
+				});
+			
+			Assert.AreEqual(new Dictionary<char, int>
+							{
+								{'f', 0},
+								{'h', 1},
+								{'g', 2},
+								{'i', 3}
+							},
+							flagCollection.Flags);
+			
+			Assert.AreEqual(new List<byte>
+							{
+								0b01100000
+							},
+							flagCollection.FlagValues);
+			
+			Assert.AreEqual(4, flagCollection.NextFreePosition);
+		}
+
+		[Test]
+		public void EmptyCreateTest()
+		{
+			var flagCollection = new FlagCollection();
+			Assert.AreEqual(new Dictionary<char, int>(), flagCollection.Flags);
+			Assert.AreEqual(new List<byte>(), flagCollection.FlagValues);
+			Assert.AreEqual(0, flagCollection.NextFreePosition);
+		}
+
+		[Test]
+		public void StringParseCreateTest()
+		{
+			// g should be ignored as it is not in possibleValues!
+			// ReSharper disable once StringLiteralTypo
+			var flagCollection = new FlagCollection("fhgi", "hgv");
+			
+			Assert.AreEqual(new Dictionary<char, int>
+							{
+								{'f', 0},
+								{'h', 1},
+								{'g', 2},
+								{'i', 3}
+							},
+							flagCollection.Flags);
+			
+			Assert.AreEqual(new List<byte>
+							{
+								0b01100000
+							},
+							flagCollection.FlagValues);
+			
+			Assert.AreEqual(4, flagCollection.NextFreePosition);
+		}
+
+		[Test]
+		public void AddTest()
+		{
+			var flagCollection = new FlagCollection()
+			{
+				Flags            = {{'h', 0}},
+				FlagValues       = {0b10000000},
+				NextFreePosition = 1
+			};
+			
+			flagCollection.SetOrAddFlag('f', true);
+			flagCollection.SetOrAddFlag('h', false);
+			
+			Assert.AreEqual(new Dictionary<char, int>
+							{
+								{'h', 0},
+								{'f', 1}
+							},
+							flagCollection.Flags);
+
+			Assert.AreEqual(new List<byte> { 0b01000000 }, flagCollection.FlagValues);
+			
+			Assert.AreEqual(2, flagCollection.NextFreePosition);
+		}
+	}
+}
---
+++ Flagger/FlagCollection.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Runtime.CompilerServices;
+
+// haha making all internal vars available for testing is a GREAT idea
+[assembly: InternalsVisibleTo("Flagger.Tests")]
+
+namespace Flagger
+{
+	// ReSharper disable once UnusedType.Global
+	public class FlagCollection
+	{
+		// internal not private so that, at the expense of being available when used inside of the Flagger namespace,
+		// these values are accessible to any unit tests in the Flagger.Tests namespace.
+		internal readonly Dictionary<char, int> Flags     = new();
+		internal readonly List<byte>            FlagValues = new();
+
+		internal int NextFreePosition;
+
+		/// <summary>
+		/// Creates an empty flag collection
+		/// </summary>
+		public FlagCollection(){}
+		
+		/// <summary>
+		/// Creates a flag collection from a given array of chars and bools
+		/// </summary>
+		/// <param name="flags">An array of flags</param>
+		public FlagCollection((char, bool)[] flags)
+		{
+			foreach (var (flagChar, value) in flags) SetOrAddFlag(flagChar, value);
+		}
+
+		/// <summary>
+		/// Creates a flag collection from a string containing every possible flag and a string of true flags - intended for command line parsers
+		/// </summary>
+		/// <param name="possibleFlags">Every possible flag</param>
+		/// <param name="givenFlags">Flags that are set true - any not in possibleFlags are ignored</param>
+		public FlagCollection(string possibleFlags, string givenFlags)
+		{
+			// Use a hash set for better performance with lots of flags!
+			var givenSet = givenFlags.ToImmutableHashSet();
+			foreach (var flag in possibleFlags) SetOrAddFlag(flag, givenSet.Contains(flag));
+		}
+
+		public bool Contains(char flag) => Flags.ContainsKey(flag);
+		public bool GetFlag(char  flag) => GetFlag(Flags[flag]);
+		
+		public bool GetFlag(int index)
+		{
+			var byteIndex = index / 8;
+			var bitIndex  = index % 8;
+			var b         = FlagValues[byteIndex];
+			var bit       = 1 << bitIndex;
+			var masked    = b & (byte) bit;
+			return masked != 0;
+		}
+
+		public void SetFlag(int index, bool value)
+		{
+			var byteIndex = index / 8;
+			var bitIndex  = index % 8;
+			var bit       = 1 << (7 - bitIndex);
+			if (value)
+				FlagValues[byteIndex] |= (byte) bit;
+			else
+				FlagValues[byteIndex] &= (byte) ~bit;
+		}
+
+		public void SetOrAddFlag(char flag, bool value)
+		{
+			if (Contains(flag))
+			{
+				SetFlag(Flags[flag], value);
+				return;
+			}
+
+			var newI = NextFreePosition;
+			Flags[flag] = newI;
+
+			while (newI / 8 >= FlagValues.Count)
+				FlagValues.Add(0);
+
+			SetFlag(newI, value);
+			
+			NextFreePosition++;
+		}
+	}
+}
